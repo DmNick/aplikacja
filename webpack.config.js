964a4e1d53d930e274e4d053f8c008c02c20d01f
DmNick <dmnickb@gmail.com>
2023-03-13 22:02:56 +0100

    instalacja yarn i modułów

--- webpack.config.js
+++ webpack.config.js
@@ -57,11 +57,11 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
-    //.enableTypeScriptLoader()
-
+    .enableTypeScriptLoader()
+    .enableStylusLoader()
     // uncomment if you use React
     //.enableReactPreset()
 
