cba599c0df6adbca6397f91ad6af901d49983325
Dominik Banik <dmnickb@gmail.com>
2023-03-15 15:53:14 +0100

    rejestracja

---
+++ README.txt
@@ -0,0 +1,4 @@
+start serwer bez logów
+symfony server:start -d
+symfony run -d yarn encore dev --watch
+symfony run -d yarn encore dev-server
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,20 +1,38 @@
 <?php
 
-namespace App\Security;
+namespace App\Entity;
 
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
-    private $email;
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
 
-    private $roles = [];
+    #[ORM\Column(length: 180, unique: true)]
+    private ?string $email = null;
+
+    #[ORM\Column]
+    private array $roles = [];
 
     /**
      * @var string The hashed password
      */
-    private $password;
+    #[ORM\Column]
+    private ?string $password = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
 
     public function getEmail(): ?string
     {
@@ -46,7 +64,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
         $roles[] = 'ROLE_USER';
-        //$roles[] = 'ROLE_ADMIN';
 
         return array_unique($roles);
     }
