fd8df0e1697e38e427930d4a72b8f25850aab6c1
Dominik Banik <dmnickb@gmail.com>
2023-03-16 15:55:28 +0100

    logowanie i rejestracja

--- README.txt
+++ README.txt
@@ -1,4 +1,4 @@
-start serwer bez logów
+//start serwera bez logów
 symfony server:start -d
 symfony run -d yarn encore dev --watch
 symfony run -d yarn encore dev-server
--- assets/app.js
+++ assets/app.js
@@ -9,7 +9,7 @@
 import './styles/app.scss';
 
 // start the Stimulus application
-import './bootstrap';
+import './bootstrap.js';
 const $ = require('jquery');
 import 'bootstrap';
 console.log("test");
